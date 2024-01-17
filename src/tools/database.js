3d7e4ae587cce92964423ea98dffcf83186c6560
Ilham Dohaan <doha39anilham@gmail.com>
2024-01-17 14:46:04 +0700

    first commit

---
+++ src/tools/database.js
@@ -0,0 +1,26 @@
+const {Sequelize} = require('sequelize');
+require('dotenv').config();
+
+module.exports = new Sequelize(
+    process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+        host: process.env.DATABASE_SERVER,
+        port: process.env.DATABASE_PORT,
+        logging: (msg) => {
+            if (msg.includes('ERROR')) {
+                logger.error(msg);
+            }
+        },
+        dialect: 'mysql',
+        dialectOptions: {
+            connectTimeout: 3000,
+            timezone: '+07:00',
+        },
+        timezone: '+07:00',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+)
